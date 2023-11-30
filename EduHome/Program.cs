1c00ae6580e85f70db3525b9097d4c28de7133c7
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-11-30 20:46:51 +0400

    Database Connection Added

--- EduHome/Program.cs
+++ EduHome/Program.cs
@@ -1,7 +1,14 @@
+using EduHome.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
