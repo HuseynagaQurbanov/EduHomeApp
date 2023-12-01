ca8e4329b53ed7d935e267e495784816e96f5dc5
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-12-01 14:38:32 +0400

    Events front to back is done

--- EduHome/Controllers/EventController.cs
+++ EduHome/Controllers/EventController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EduHome.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHome.Controllers
 {
 	public class EventController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+        private readonly AppDbContext _context;
+
+        public EventController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var events = _context.Events.ToList();
+
+            return View(events);
+        }
+
+        public IActionResult EventDetail(Guid detailUrl)
+        {
+            var eventItem = _context.Events.Include(s => s.Speakers).FirstOrDefault(e => e.Id == detailUrl);
+
+            return View(eventItem);
+        }
+    }
 }
