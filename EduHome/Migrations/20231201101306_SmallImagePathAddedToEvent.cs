560d7e18ede839422d75b52405d8312e98f4c73c
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-12-01 14:13:37 +0400

    SmallImagePath added to Event

---
+++ EduHome/Migrations/20231201101306_SmallImagePathAddedToEvent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduHome.Migrations
+{
+    public partial class SmallImagePathAddedToEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SmallImagePath",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SmallImagePath",
+                table: "Events");
+        }
+    }
+}
--- EduHome/Models/Event.cs
+++ EduHome/Models/Event.cs
@@ -3,7 +3,8 @@
 	public class Event : BaseEntity
 	{
 		public string Name { get; set; }
-		public string ImagePath { get; set; }
+        public string SmallImagePath { get; set; }
+        public string ImagePath { get; set; }
 		public DateTime StartTime { get; set; }
 		public DateTime EndTime { get; set; }
 		public string Venue { get; set; }
